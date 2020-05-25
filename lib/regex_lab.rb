71667870d9a7f2bfdf5b783a48bd3307d661c272
Jacob W Jones <jacobwadejones76@gmail.com>
2020-05-25 19:57:30 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,6 @@
 def starts_with_a_vowel?(word)
- if word.scan(/^\[aeiouAEIOU]\g/)
-   true
- else 
-   false
- end
+  !!word.capitalize.match(/\A+[AEIOU]/)
+
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
